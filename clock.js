79336f5e79c1ee406d34d448f312c59a5243bbaa
Hakase <hakase@litdevs.org>
2024-06-23 12:27:52 +0200

    tweaks

---
+++ clock.js
@@ -0,0 +1,22 @@
+document.write("<div id='clock'><span class='time'><span class='hour'>12</span><span class='colon'>:</span><span class='minute'>34</span><span class='colon'>:</span><span class='second'>56</span></span></div>");
+document.addEventListener("DOMContentLoaded", function() {
+    function updateClock() {
+        const time = new Date().toLocaleTimeString('no-NB', { timeZone: 'Europe/Oslo' });
+        const currentTime = {
+            hour: time.split(":")[0],
+            minute: time.split(":")[1],
+            second: time.split(":")[2],
+        }
+        document.querySelector("#clock .time .hour").innerText = currentTime.hour;
+        document.querySelector("#clock .time .minute").innerText = currentTime.minute;
+        document.querySelector("#clock .time .second").innerText = currentTime.second;
+
+        if(currentTime.second % 2) {
+            document.querySelector("#clock .time").classList.add("showcolons");
+        } else {
+            document.querySelector("#clock .time").classList.remove("showcolons");
+        }
+    }
+    updateClock();
+    setInterval(updateClock,500)
+})
