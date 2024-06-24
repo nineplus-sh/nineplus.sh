de5f3ea719847434461f2ef939bb7824c6081c89
Hakase <hakase@litdevs.org>
2024-06-24 21:19:53 +0200

    need to figure out the logos sometime

--- clock.js
+++ clock.js
@@ -1,4 +1,4 @@
-document.write("<div id='clock'><span class='time'><span class='hour'>12</span><span class='colon'>:</span><span class='minute'>34</span><span class='colon'>:</span><span class='second'>56</span></span></div>");
+document.write("<div id='clock' aria-hidden='true'><span class='time'><span class='hour'>12</span><span class='colon'>:</span><span class='minute'>34</span><span class='colon'>:</span><span class='second'>56</span></span></div>");
 document.addEventListener("DOMContentLoaded", function() {
     function updateClock() {
         const time = new Date().toLocaleTimeString('no-NB', { timeZone: 'Europe/Oslo' });
